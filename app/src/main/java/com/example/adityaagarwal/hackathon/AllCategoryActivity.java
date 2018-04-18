f318dc0bfb2886419ecb98c6b72d44ab557794fe
Aditya Agarwal <aditya.agarwal@tesco.com>
2018-04-18 21:41:45 +0530

    selected categories

--- app/src/main/java/com/example/adityaagarwal/hackathon/AllCategoryActivity.java
+++ app/src/main/java/com/example/adityaagarwal/hackathon/AllCategoryActivity.java
@@ -1,9 +1,11 @@
 package com.example.adityaagarwal.hackathon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,9 @@ public class AllCategoryActivity extends AppCompatActivity {
 
     @BindView(R.id.recycler_view)
     RecyclerView recyclerView;
+
+    @BindView(R.id.done)
+    TextView done;
 
     private AllCategoryAdapter adapter;
     List<String> categoryList = new ArrayList<>();
@@ -37,6 +42,12 @@ public class AllCategoryActivity extends AppCompatActivity {
 
         adapter.setClickListener(clickListener);
         adapter.setCategoriesList(categoryList);
+
+        done.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TripActivity.class);
+            startActivity(intent);
+        });
+
     }
 
     private void createCategoryList() {
